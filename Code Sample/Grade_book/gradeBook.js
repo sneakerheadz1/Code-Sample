95345f7447368d38cf3b2addf0a2f5a57a079323
Dom <55361342+sneakerheadz1@users.noreply.github.com>
2021-08-02 13:12:52 -0700

    Add files via upload

---
+++ Code Sample/Grade_book/gradeBook.js
@@ -0,0 +1,22 @@
+//  Grade Book
+
+
+// var userGrade = window.prompt("Please enter your grade: ");
+
+var studentGrade = 54
+
+if (studentGrade >= 90){
+    console.log(" You have an A!")
+}
+else if(studentGrade >= 80){
+    console.log("You have an B")
+}
+else if (studentGrade >= 70){
+    console.log("You have an C")
+}
+else if(studentGrade >=55){
+    console.log("You have an D, schedule retake")
+}
+else{
+    console.log("You have an F, Schedule retake")
+}
---
+++ Code Sample/conditionals/app.js
@@ -0,0 +1,57 @@
+//  Conditionals 
+//  Conditionals test to see if something is true
+
+var coding = 0
+
+// if  (coding == true){  /* condition is in the parentheses*/
+//     //  condition to be used if condition is true
+//     console.log("I am happy to be coding!!!")
+// }
+// else{
+//     console.log("I wish I was coding!!!")
+// }
+
+//  Modulo is the % (provides the remainder os divided numbers)
+
+// var numToBeTested = 10;
+
+// if (numToBeTested % 2 === 0){
+//     console.log('this number is even')
+
+//     if (numToBeTested <= 100){
+//         console.log("Please choose a higher number..")
+//     }
+// }
+// else{
+//     console.log('this number is odd')
+// }
+
+var userAge = 17;
+
+if (userAge >= 18){
+    console.log("Welcome in the club, but no drinking..")
+    
+        if (userAge >=  21){
+          console.log("Welcome in the club..")
+        }
+}
+else{
+    console.log("Come back when you are 18...")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// loose comparison (looks for one value to be true, either value or data type)
+
+//  strict comparison (looks at data type and value must match)
